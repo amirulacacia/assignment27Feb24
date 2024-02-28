36c01bc25c2e811aef777dc61c367eed2d7b4395
amirulacacia <amirulacacia@outlook.com>
2024-02-28 10:11:49 +0800

    28/02/2024 Adding Autoscroll on the log and disconnect button

--- src/main/resources/static/javaxwebsocket/websocket.js
+++ src/main/resources/static/javaxwebsocket/websocket.js
@@ -1,31 +1,59 @@
 
 var ws, ws2;
 
+function setConnected(connected) {
+    document.getElementById('connect').disabled = connected;
+    document.getElementById('disconnect').disabled = !connected;
+    document.getElementById('send').disabled = !connected;
+}
+
 function connect() {
     var username = document.getElementById("username").value;
     var host = document.location.host;
     ws = new WebSocket("ws://" + host + "/chat/" + username);
 
-    // ws.onmessage = function(event){
-    //     console.log(event);
-    //     var message = JSON.parse(event.data);
-    //     log.innerHTML += message.from + " : " + message.content + "\n";
-    // }
-
+    ws.onopen = function () {
+        setConnected(true);
+        log.innerHTML += "You are online" + "\n";
+        log2.innerHTML += username + " is online" + "\n";
+        autoScroll();
+    };
     ws.onmessage = function (event) {
         console.log(event);
         var message = event.data;
         log.innerHTML += message + "\n";
         log2.innerHTML += message + "\n";
+        autoScroll();
     }
+    ws.onclose = function () {
+        setConnected(false);
+        log.innerHTML += "You are offline" + "\n";
+        log2.innerHTML += username + " is offline" + "\n";
+        autoScroll();
+    };
+}
+
+function disconnect() {
+    if (ws != null) {
+        ws.close();
+        ws = null;
+    }
+    setConnected(false);
 }
 
 function send() {
-    var content = document.getElementById("msg").value;
-    // var json = JSON.stringify({
-    //     "content":content
-    // });
-    ws.send(content);
+    if (ws != null) {
+        var content = document.getElementById("msg").value;
+        ws.send(content);
+    } else {
+        alert('WebSocket connection not established, please connect.');
+    }
+}
+
+function setConnected2(connected) {
+    document.getElementById('connect2').disabled = connected;
+    document.getElementById('disconnect2').disabled = !connected;
+    document.getElementById('send2').disabled = !connected;
 }
 
 function connect2() {
@@ -33,25 +61,52 @@ function connect2() {
     var host = document.location.host;
     ws2 = new WebSocket("ws://" + host + "/chat/" + username);
 
-    // ws.onmessage = function(event){
-    //     console.log(event);
-    //     var message = JSON.parse(event.data);
-    //     log.innerHTML += message.from + " : " + message.content + "\n";
-    // }
-
+    ws2.onopen = function () {
+        setConnected2(true);
+        log2.innerHTML += "You are online" + "\n";
+        log.innerHTML += username + " is online" + "\n";
+        autoScroll();
+    };
     ws2.onmessage = function (event) {
         console.log(event);
         var message = event.data;
         log.innerHTML += message + "\n";
         log2.innerHTML += message + "\n";
+        autoScroll();
     }
+    ws2.onclose = function () {
+        setConnected2(false);
+        log2.innerHTML += "You are offline" + "\n";
+        log.innerHTML += username + " is offline" + "\n";
+        autoScroll();
+    };
+}
+
+function disconnect2() {
+    if (ws2 != null) {
+        ws2.close();
+        ws2 = null;
+    }
+    setConnected2(false);
 }
 
 function send2() {
-    var content = document.getElementById("msg2").value;
-    // var json = JSON.stringify({
-    //     "content":content
-    // });
-    ws2.send(content);
+    if (ws2 != null) {
+        var content = document.getElementById("msg2").value;
+        ws2.send(content);
+    } else {
+        alert('WebSocket connection not established, please connect.');
+    }
+}
+
+// Get the textarea element
+var textarea = document.getElementById('log');
+var textarea2 = document.getElementById('log2');
+
+// Function to auto-scroll the textarea
+function autoScroll() {
+  // Set the scrollTop property to the maximum height to auto-scroll
+  textarea.scrollTop = textarea.scrollHeight;
+  textarea2.scrollTop = textarea2.scrollHeight;
 }
 
